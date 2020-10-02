c346583c0e312603da667816bee4c3e4dd5893c7
mohammed ismail <warethco@gmail.com>
2020-10-02 17:36:54 +0200

    Initial Author Routes

--- server.js
+++ server.js
@@ -6,17 +6,20 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 mongoose.connect(process.env.DATABASE_URL, {
     useUnifiedTopology: true,
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useCreateIndex: true
 })
-.then(() => console.log('Connected to Mongo')).catch(err => console.log(err))
+    .then(() => console.log('Connected to Mongo')).catch(err => console.log(err))
 const db = mongoose.connection
 db.on('error', err => console.log(err))
 
 // routes
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/author')
 
 
 app.set('view engine', 'ejs')
@@ -24,8 +27,10 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 // routes
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000, () => console.log('server is running'))
